2e33236767d2437beb85e65fabe4c22878e80e77
changyuchun <changyuchun@inke.cn>
2021-10-27 20:47:20 +0800

    全局跨域设置

--- src/main/java/com/example/myspring2021/controller/DepartController.java
+++ src/main/java/com/example/myspring2021/controller/DepartController.java
@@ -6,13 +6,12 @@ import com.example.myspring2021.service.DepartServiceImpl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
+//import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.annotation.Resource;
 import java.util.List;
 
-@CrossOrigin // 只有指定域名可以访问该类下所有接口
+//@CrossOrigin // 只有指定域名可以访问该类下所有接口
 @RestController
 @RequestMapping("/depart")
 public class DepartController {
